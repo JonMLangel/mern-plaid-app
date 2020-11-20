2667ff0dbcc01db4bb8b7a197559ede8f68205af
Jonathon Langel <jonathon.langel@zaneray.com>
2020-11-20 09:44:52 -0600

    partially completed routes

---
+++ client/src/actions/accountActions.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import {
+  ADD_ACCOUNT,
+  DELETE_ACCOUNT,
+  GET_ACCOUNTS,
+  ACCOUNTS_LOADING,
+  GET_TRANSACTIONS,
+  TRANSACTIONS_LOADING
+} from "./types";
+
+// Actions will go here
+
+// Add account
+export const addAccount = plaidData => dispatch => {
+    const accounts = plaidData.accounts;
+    axios
+      .post("/api/plaid/accounts/add", plaidData)
+      .then(res =>
+        dispatch({
+          type: ADD_ACCOUNT,
+          payload: res.data
+        })
+      )
+      .then(data =>
+        accounts ? dispatch(getTransactions(accounts.concat(data.payload))) : null
+      )
+      .catch(err => console.log(err));
+  };
+
+  // Delete account
+export const deleteAccount = plaidData => dispatch => {
+    if (window.confirm("Are you sure you want to remove this account?")) {
+      const id = plaidData.id;
+      const newAccounts = plaidData.accounts.filter(
+        account => account._id !== id
+      );
+      axios
+        .delete(`/api/plaid/accounts/${id}`)
+        .then(res =>
+          dispatch({
+            type: DELETE_ACCOUNT,
+            payload: id
+          })
+        )
+        .then(newAccounts ? dispatch(getTransactions(newAccounts)) : null)
+        .catch(err => console.log(err));
+    }
+  };
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
+  mongoURI: "mongodb+srv://admin:admin@cluster0.xvdfa.mongodb.net/<dbname>?retryWrites=true&w=majority/mern-plaid-app",
   secretOrKey: "secret"
 };
